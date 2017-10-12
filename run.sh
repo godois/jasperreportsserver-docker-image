2a3cf410722e87a179e1de430c0fea0c260363fd
Marcio Godoi <souzagodoi@gmail.com>
2017-10-12 20:49:18 -0300

    Organizing the jasperreport installation and tomcat start up

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "Jasper Reports Server is starting ... "
+if [ "$NEWINSTANCE" = true ];
+then
+    echo "JasperReports Server is beeing installed... "
+	./tmp/jasper/jasperreports-server-cp-6.4.0-bin/buildomatic/js-install-ce.sh
+	
+fi
 
-
-
+echo "JasperReports Server is starting... "
+./usr/local/tomcat/bin/startup.sh
